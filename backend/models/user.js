c3cda0b20dba9c1667f7033d02eeacebe7859440
absurd-web <2018infor18@gmail.com>
2022-06-11 12:24:34 -0300

    backend e login adicionado

---
+++ backend/models/user.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database.js');
+
+const User = sequelize.define('usuarios', {
+   UsuarioID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+   },
+   NomeUsuario: {
+      type: Sequelize.STRING,
+      allowNull: false,
+   },
+   Email: {
+      type: Sequelize.STRING,
+   },
+   Senha: {
+      type: Sequelize.STRING,
+      allowNull: false,
+   },
+   SenhaSal: {
+      type: Sequelize.STRING,
+      allowNull: true,
+   },
+   SenhaHashUsado: {
+      type: Sequelize.STRING,
+      allowNull: true,
+   }},
+   {
+      timestamps: false,
+});
+
+module.exports = User;
